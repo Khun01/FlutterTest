2cac3e85b27a699c72d1038a217f0a199818653d
Mariecher V <mariecherv.@Mariechers-MacBook-Air.local>
2024-08-30 14:56:03 +0800

    hehe

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -42,13 +42,9 @@ class LoginPage extends StatelessWidget {
             builder: (context, state) {
               return Stack(
                 children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height,
-                    child: Image.asset(
-                      'assets/images/login_bg.png',
-                      fit: BoxFit.cover,
-                    ),
+                  Image.asset(
+                    'assets/images/login_bg.png',
+                    fit: BoxFit.cover,
                   ),
                   Positioned(
                     top: 16,
@@ -63,20 +59,21 @@ class LoginPage extends StatelessWidget {
                       ),
                     ),
                   ),
+                  Positioned(
+                    top: 35,
+                    left: 100,
+                    right: 100,
+                    child: Image.asset(
+                      'assets/images/upang_logo.png',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                   Container(
-                    margin: const EdgeInsets.only(top: 24, left: 24, right: 24),
-                    width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height,
+                    margin: const EdgeInsets.only(top: 100, left: 24, right: 24),
                     child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Center(
-                          child: Image.asset(
-                            'assets/images/upang_logo.png',
-                            width: 200,
-                            fit: BoxFit.contain,
-                          ),
-                        ),
                         const SizedBox(height: 48),
                         Text(
                           role!,
